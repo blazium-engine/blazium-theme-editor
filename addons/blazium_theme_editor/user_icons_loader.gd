a553fd7eaf1b8ebb00c1674c97cf45cb344d72d0
Whales State <whalesstate@gmail.com>
2025-02-26 02:17:32 +0200

    fix icons loader in exported games.

--- addons/blazium_theme_editor/user_icons_loader.gd
+++ addons/blazium_theme_editor/user_icons_loader.gd
@@ -2,7 +2,6 @@ extends Node
 
 
 func _init() -> void:
-	var icons_path := BlaziumThemePlugin.singleton.ICONS_PATH
-	var icons: Dictionary = ProjectSettings.get_setting(icons_path, {})
+	var icons: Dictionary = ProjectSettings.get_setting("gui/theme/user_icons", {})
 	for icon in icons:
 		ThemeDB.add_user_icon(icon, icons[icon])
